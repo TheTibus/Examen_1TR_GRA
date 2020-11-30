c868149f4e3ea8b65cb9ba106d807ad35e980850
TheTibus <72516070+TheTibus@users.noreply.github.com>
2020-11-30 17:01:48 +0100

    Script creación de Columnas con comprobador DONE

---
+++ Adrian Gil/Assets/Scripts/CreadorColumnas.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorColumnas : MonoBehaviour
+{
+    public int numeroColumnas = 0;
+    [SerializeField] GameObject columnas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ColumnCorrutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void CrearColumna()
+    {
+
+        float posRandom = Random.Range(-34, 34);
+        float posRandom2 = Random.Range(-34, 34);
+
+        Vector3 DestPos = new Vector3(posRandom, 0, posRandom2);
+        Instantiate(columnas, DestPos, Quaternion.identity);
+        numeroColumnas ++;
+        print(numeroColumnas);
+    }
+   
+    IEnumerator ColumnCorrutine()
+    {
+        for(int n = 0; ; n++)
+        {
+            CrearColumna();
+           
+            if (numeroColumnas <= 5)
+            {
+                print("Pasan 1 segundos");
+                yield return new WaitForSeconds(1);
+               
+            }
+            else if (numeroColumnas <= 10)
+            {
+                print("Pasan 5 segundos");
+                yield return new WaitForSeconds(5);
+                
+            }
+            else
+            {
+                print("Pasan 10 segundos");
+                yield return new WaitForSeconds(10);
+                
+            }
+
+        }
+    }
+    
+}
