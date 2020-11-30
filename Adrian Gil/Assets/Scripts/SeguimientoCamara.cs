19562dab344d1fb47f283130d23040acf496f5f5
TheTibus <72516070+TheTibus@users.noreply.github.com>
2020-11-30 16:24:08 +0100

    Seguimiento Cámara DONE

---
+++ Adrian Gil/Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour
+{
+
+    public Transform posicionJugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(posicionJugador.position.x, posicionJugador.position.y + 40f, posicionJugador.position.z - 15f);
+
+
+    }
+}
