66ab46f3e1f58e938a3699a3a2cf02ef56352552
TheTibus <72516070+TheTibus@users.noreply.github.com>
2020-11-30 19:26:37 +0100

    Script Timer DONE

---
+++ Adrian Gil/Assets/Scripts/Tiempo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tiempo : MonoBehaviour
+{
+    
+    [SerializeField] Text TimePlayed;
+    float tiempo;
+    float minutos;
+    float segundos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update() {
+
+        UIMarcadores();
+    }
+    
+
+    
+
+void UIMarcadores()
+{
+    tiempo += Time.deltaTime;
+    segundos = (int)tiempo % 60;
+    minutos = (int)((tiempo / 60) % 60);
+    TimePlayed.text = "Tiempo: " + minutos.ToString("00") + ":" + segundos.ToString("00");
+   
+
+}
+}
